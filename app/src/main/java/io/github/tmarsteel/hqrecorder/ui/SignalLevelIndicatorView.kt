a160f3276b94cb46e7a35430bc538de334f5087d
Tobias Marstaller <tobias.marstaller@proton.me>
2025-09-27 23:26:48 +0200

    fix initialization bug in SignalLevelIndicatorView

--- app/src/main/java/io/github/tmarsteel/hqrecorder/ui/SignalLevelIndicatorView.kt
+++ app/src/main/java/io/github/tmarsteel/hqrecorder/ui/SignalLevelIndicatorView.kt
@@ -13,9 +13,10 @@ import io.github.tmarsteel.hqrecorder.util.getSampleLevelAsDecibelText
 import kotlin.math.absoluteValue
 
 class SignalLevelIndicatorView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
-    private lateinit var measuredTextChannelIndicator: MeasuredText
-    private lateinit var measuredTextPeakSignalLevel: MeasuredText
-    private lateinit var peakSignalLevelText: String
+    private var measuredTextChannelIndicator: MeasuredText = MeasuredText.Builder(charArrayOf())
+        .build()
+    private var measuredTextPeakSignalLevel: MeasuredText = measuredTextChannelIndicator
+    private var peakSignalLevelText: String = getSampleLevelAsDecibelText(0.0f)
 
     var channelIndicator: String = "L"
         set(value) {
@@ -105,7 +106,6 @@ class SignalLevelIndicatorView(context: Context, attrs: AttributeSet? = null) : 
         channelIndicator = channelIndicator
         clipIndicator = clipIndicator
     }
-
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawPaint(paintBackground)
